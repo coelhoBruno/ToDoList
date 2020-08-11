36e4df57ef8d26ed716e301da42172cdafe1e6ce
Bruno Coelho da Silva <coelho1_bruno@hotmail.com>
2020-08-11 19:48:30 -0300

    Add GET

--- src/controllers/todo.js
+++ src/controllers/todo.js
@@ -1,3 +1,7 @@
+exports.get = (req, res, next) => {
+    res.status(201).send('Requisição recebida com sucesso!');
+};
+
 exports.post = (req, res, next) => {
     res.status(201).send('Requisição recebida com sucesso!');
 };
